e9df97fe678cd7d943b1a18818717cce702ec5c4
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-12-12 20:02:51 -0600

    working on this one again and got a question to show up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,121 @@
 var startButton = document.getElementById('start');
-var questionElement = document.getElementById('question-element');
-var questions = document.getElementById('question-container');
+var questionsCon = document.getElementById('question-container');
 var answerButtons = document.getElementById('answer-buttons');
+var timer = document.getElementById('timer')
+var changeButton = document.getElementById('ChangeButton');
+var answerButton = document.getElementsByClassName('answer-button');
 
-var questions = [
+// // var score = questions[0];
+var time = 60;
+// var currentTimer;
+// var currentQuestion = -1;
+
+function runTimer(){
+currentTimer= setTimeout(() => {
+        time--
+        timer.innerHTML = time
+        if(time > 0){
+            runTimer()
+        } else {
+            stopTimer()
+        }
+    }, 1000);
+}
+
+const questions = [
     {
-    question: 'question',
-    answers: [
-        { text: 'correct' },
-        { text: 'wrong' },
-        { text: 'wrong' },
-        { text: 'wrong' }
-    ]
-    },
-    {
-        question: 'question',
+        question: "Commonly used data types do NOT include",
         answers: [
-        { text: 'correct' },
-        { text: 'wrong' },
-        { text: 'wrong' },
-        { text: 'wrong' }
+            { text: "alerts", correct: true},
+            { text: "booleans", correct: false},
+            { text: "strings", correct: false},
+            { text: "square bracket", correct: false}
         ]
     },
     {
-        question: 'question',
+        question: "The condition in an if/else statement is enclosed within ____.",
         answers: [
-        { text: 'correct' },
-        { text: 'wrong' },
-        { text: 'wrong' },
-        { text: 'wrong' }
+            { text: "quotes", correct: false},
+            { text: "curly brackets", correct: false},
+            { text: "parenthesis", correct: true},
+            { text: "square brackets", correct: false}
+        ]
+    },
+    {
+        question: "Which is a data type?",
+        answers: [
+            { text: "function", correct: false},
+            { text: "boolean", correct: true},
+            { text: "variable", correct: false},
+            { text: "message", correct: false}
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        answers: [
+            { text: "other arrays", correct: false},
+            { text: "boolean", correct: false},
+            { text: "numbers and strings", correct: false},
+            { text: "all of the above", correct: true}
         ]
     },
 ]
 
-var answers = []
-var score = ""
+let finalQuestion = questions.length
+let answer1 = document.getElementById('ans1');
+let answer2 = document.getElementById('ans2');
+let answer3 = document.getElementById('ans3');
+let answer4 = document.getElementById('ans4');
+let questionEl = document.getElementById('question');
+console.log(questions[0].answers[0].text)
+function showQuestions(){
+    if (finalQuestion > 0){
+        questionEl.textContent = questions[0].question;
+        answer1.textContent = questions[0].answers[0].text;
+        answer2.textContent = questions[0].answers[1].text;
+        answer3.textContent = questions[0].answers[2].text;
+        answer4.textContent = questions[0].answers[3].text;
+    }
 
+}
 
 function startQuiz() {
     startButton.classList.add('hide');
     document.getElementById('question-container').classList.remove('hide');
+    runTimer();
+    console.log('start');
+    showQuestions()
 
-    
+    // for (i=0; )
+
+
+nextQuestion();
+
 }
+
+function nextQuestion(){
+    currentQuestion++
+    if (currentQuestion > questions.length){
+        showScore();
+    } else {
+        questionsCon.innerHTML= questions[currentQuestion].text
+    }
+}
+
+
+
+
+// function showQuestionAndAnswer(){
+//     questionEl.text = 
+//     answer0.text =  
+//     answer1.text =  
+//     answer2.text = 
+//     answer3.text = 
+// }
+
+
+
+
 
 startButton.addEventListener('click',startQuiz);
 
